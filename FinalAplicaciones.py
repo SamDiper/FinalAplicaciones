cfc09dea11633c6813ab166b9d4c1bca74788f0f
SamDiper <samueldiazperez4c@gmail.com>
2023-11-24 22:26:40 -0500

    Se hace el cambio 2 con la funcion move_piece

--- FinalAplicaciones.py
+++ FinalAplicaciones.py
@@ -35,8 +35,12 @@ class ChessGame(tk.Tk):
         else:
             self.move_piece(row, col)
                 
-    #Cambio1
-    def refresh_board(self):
-            for widget in self.winfo_children():
-                widget.destroy()
-            self.create_board()
+    
+    #Cambio2
+    def move_piece(self, row, col):
+        selected_row, selected_col = self.selected_piece
+        piece = self.board[selected_row][selected_col]
+        self.board[selected_row][selected_col] = ' '
+        self.board[row][col] = piece
+        self.selected_piece = None
+        self.refresh_board()
